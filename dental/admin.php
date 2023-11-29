453f3807478b8ff0722d950f832536c4b214ffb4
mahadiktejas2003 <mahadiktejas165@gmail.com>
2023-11-29 14:44:14 +0530

    updated admin portal frontend, and edit staff tabl

--- dental/admin.php
+++ dental/admin.php
@@ -1,16 +1,141 @@
+<!-- admin.php -->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <link rel="stylesheet" type="text/css" href="admin.css">
     <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <style>
+        /* Additional styles for heading and buttons */
+        h1 {
+            font-family: 'Lobster', cursive;
+            font-weight: bold;
+            font-size: 36px;
+            text-align: center;
+            background: linear-gradient(to right, #048FFF, #88F7F9);
+            color: #ffffff;
+            margin: 0;
+            padding: 20px;
+            opacity: 0.9; /* Adjust opacity as needed */
+        }
+
+        /* Logout button styles */
+        .logout {
+            box-sizing: border-box;
+            border: none;
+            background-color: #207db5; /* Adjust color as needed */
+            color: #ffffff; /* Adjust color as needed */
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            border-radius: 8px;
+            position: absolute;
+            top: 20px;
+            right: 40px;
+        }
+
+        .logout:hover {
+            background-color: #185c84; /* Adjust color as needed */
+            text-decoration: underline;
+        }
+
+        /* Navigation button styles */
+        .sublink,
+        .sublinka,
+        .bottoma {
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            display: inline-block;
+            width: auto;
+            padding: 10px 20px;
+            text-align: center;
+            margin: 0 10px 10px 0;
+            text-decoration: none;
+            color: white;
+            background-color: #2b72e8;
+        }
+
+        .sublinka:hover,
+        .sublink:hover,
+        .bottoma:hover {
+            background-color: #4197ee;
+        }
+
+        /* Image styles */
+        .img {
+            position: absolute;
+            margin-top: 80px;
+            margin-left: 310px;
+        }
+
+        /* Right and Left container styles */
+        .right,
+        .left {
+            position: absolute;
+            margin-top: 200px;
+            background-color: #2b72e8;
+            padding: 40px 30px;
+            text-decoration: none;
+        }
+
+        /* Additional styles for specific buttons */
+        .l1,
+        .l2,
+        .l3,
+        .l4,
+        .r1,
+        .r2,
+        .r3,
+        .r4 {
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            display: inline-block;
+            width: auto;
+            padding: 10px 20px;
+            text-align: center;
+            margin: 0 10px 10px 0;
+            text-decoration: none;
+            color: white;
+            background-color: #2b72e8;
+        }
+
+        .l1:hover,
+        .l2:hover,
+        .l3:hover,
+        .l4:hover,
+        .r1:hover,
+        .r2:hover,
+        .r3:hover,
+        .r4:hover {
+            background-color: #4197ee;
+        }
+
+        .sublink.r4 {
+            margin-top: 5px;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            position: relative;
+        }
+
+        .sublink.r4:hover {
+            background-color: #4197ee;
+        }
+    </style>
 </head>
 <body>
+    <h1>Dental Clinic System</h1>
     <a href="index.html" class="logout">Logout</a>
     <img src="admin.jpg" class="img">
     <div class="right">
         <a href="dentisteditadmin.php" class="sublink r1">Dentist Edit</a><br><br>
         <a href="cliniceditadmin.php" class="sublink r2">Clinic Edit</a><br><br>
-        <a href="patienteditadmin.php" class="sublink r3">Patient Edit</a>
+        <a href="patienteditadmin.php" class="sublink r3">Patient Edit</a><br><br>
+        <a href="viewstaff.php" class="sublink r4">View Staff</a>
     </div>
     <div class="left">
         <a href="dentist.php" class="sublink l1">Add Dentist</a><br><br>
@@ -22,7 +147,6 @@
         <a href="adminreg.php" class="bottoma">Enter Appointment Dates</a>
         <a href="adminregview.php" class="bottoma">Change Appointment Dates</a>
         <a href="confirmappoint.php" class="bottoma">Confirm Appointment</a>
-        <!-- Update the link to point to viewstaff.php -->
         <a href="viewstaff.php" class="bottoma">View Staff</a>
     </div>
 </body>
--- dental/edit_staffinfo.php
+++ dental/edit_staffinfo.php
@@ -1,63 +1,173 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
+    <title>Edit Staff Member</title>
     <meta charset="utf-8">
-    <title>Edit Staff Information</title>
-    <link rel="stylesheet" type="text/css" href="dentisteditadmin.css">
-    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="dentist.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        #container {
+            width: 50%;
+            margin: 3cm 0 0 5cm; /* 3cm from the top, 5cm from the left */
+            background-color: #fff;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        #content {
+            text-align: left;
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        form {
+            width: 100%;
+            max-width: 400px;
+            margin: 20px 0;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+            color: #333;
+        }
+
+        input[type="text"] {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #007bff;
+            color: #fff;
+            padding: 10px;
+            border: none;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
     <div id="container">
-        <div id="content">
-            <h2 class="page">Edit Staff Information</h2>
+        <div id="content"><!-- Start of the edit page content -->
+            <h2>Edit Staff Member</h2>
 
             <?php
-            require('connect-mysql.php');
-
-            if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id'])) {
-                $staff_id = $_GET['id'];
-
-                $q = "SELECT * FROM staff WHERE sid = $staff_id";
-                $result = @mysqli_query($dbcon, $q);
-
-                if ($result) {
-                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-                    $name = $row['name'];
-                    $age = $row['age'];
-                    $sex = $row['sex'];
-                    $email = $row['email'];
-                    $address = $row['address'];
-                    $phone = $row['phone'];
-
-                    echo '<form method="post" action="update_staffinfo.php">
-                            <input type="hidden" name="id" value="' . $staff_id . '">
-                            <label>Name:</label>
-                            <input type="text" name="name" value="' . $name . '" required><br>
-                            <label>Age:</label>
-                            <input type="text" name="age" value="' . $age . '" required><br>
-                            <label>Sex:</label>
-                            <input type="text" name="sex" value="' . $sex . '" required><br>
-                            <label>Email:</label>
-                            <input type="email" name="email" value="' . $email . '" required><br>
-                            <label>Address:</label>
-                            <textarea name="address" required>' . $address . '</textarea><br>
-                            <label>Phone:</label>
-                            <input type="text" name="phone" value="' . $phone . '" required><br>
-                            <input type="submit" value="Update">
-                        </form>';
-
-                    mysqli_free_result($result);
-                } else {
-                    echo '<p class="error">Error fetching staff information.</p>';
-                    echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $q . '</p>';
+                // After clicking the Edit link in the viewstaff.php page, the editing interface appears
+                // The code looks for a valid staff ID, either through GET or POST
+                if ((isset($_GET['id'])) && (is_numeric($_GET['id']))) {
+                    $id = $_GET['id'];
+                } elseif ((isset($_POST['id'])) && (is_numeric($_POST['id']))) {
+                    $id = $_POST['id'];
+                } else { // If no valid ID, stop the script
+                    echo '<p class="error">This page has been accessed in error</p>';
+                    exit();
                 }
-            } else {
-                echo '<p class="error">Invalid request. Please go back to the <a href="viewstaff.php">View Staff page</a>.</p>';
-            }
 
-            mysqli_close($dbcon);
+                require('connect-mysql.php');
+
+                // Has the form been submitted?
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    $errors = array();
+
+                    // Look for the staff member details
+                    if (empty($_POST['name'])) {
+                        $errors[] = 'You forgot to enter the name';
+                    } else {
+                        $name = mysqli_real_escape_string($dbcon, trim($_POST['name']));
+                    }
+
+                    // Look for the age
+                    if (empty($_POST['age'])) {
+                        $errors[] = 'You forgot to enter the age.';
+                    } else {
+                        $age = mysqli_real_escape_string($dbcon, trim($_POST['age']));
+                    }
+
+                    // Look for the phone number
+                    if (empty($_POST['phone'])) {
+                        $errors[] = 'You forgot to enter the phone no.';
+                    } else {
+                        $phone = mysqli_real_escape_string($dbcon, trim($_POST['phone']));
+                    }
+
+                    // Look for the email
+                    if (empty($_POST['email'])) {
+                        $errors[] = 'You forgot to enter the email id.';
+                    } else {
+                        $email = mysqli_real_escape_string($dbcon, trim($_POST['email']));
+                    }
+
+                    // Look for the address
+                    if (empty($_POST['address'])) {
+                        $errors[] = 'You forgot to enter the address.';
+                    } else {
+                        $address = mysqli_real_escape_string($dbcon, trim($_POST['address']));
+                    }
+
+                    if (empty($errors)) { // If everything is OK, make the update query
+                        $q = "UPDATE staff SET name='$name', age='$age', phone='$phone', email='$email', address='$address' WHERE sid=$id LIMIT 1";
+                        $result = @mysqli_query($dbcon, $q);
+                        if (mysqli_affected_rows($dbcon) == 1) { // If it ran OK
+                            echo '<h3>The staff member has been edited.</h3>';
+                        } else { // If the query failed
+                            echo '<p class="error">The staff member could not be edited due to a system error. We apologize for any inconvenience.</p>';
+                            echo '<p>' . mysqli_error($dbcon) . '<br />Query: ' . $q . '</p>';
+                        }
+                        mysqli_close($dbcon);
+                        exit();
+                    } else { // Display the errors.
+                        echo '<p class="error">The following error(s) occurred:<br />';
+                        foreach ($errors as $msg) {
+                            echo " - $msg<br>\n";
+                        }
+                        echo '</p><p>Please try again.</p>';
+                    }
+                } // End of the conditionals
             ?>
-        </div>
+
+            <?php
+                // Select the record
+                $q = "SELECT sid, name, age, phone, email, address FROM staff WHERE sid=$id";
+                $result = @mysqli_query($dbcon, $q);
+                if (mysqli_num_rows($result) == 1) { // Valid staff ID, display the form.
+                    // Get the staff member's information
+                    $row = mysqli_fetch_array($result, MYSQLI_NUM);
+                    // Create the form
+                    echo '<form action="edit_staffinfo.php" method="post">
+                            <label for="name">Name:</label>
+                            <input type="text" name="name" value="' . $row[1] . '">
+                            
+                            <label for="age">Age:</label>
+                            <input type="text" name="age" value="' . $row[2] . '">
+                            
+                            <label for="phone">Phone:</label>
+                            <input type="text" name="phone" value="' . $row[3] . '">
+                            
+                            <label for="email">Email:</label>
+                            <input type="text" name="email" value="' . $row[4] . '">
+                            
+                            <label for="address">Address:</label>
+                            <input type="text" name="address" value="' . $row[5] . '">
+                            
+                            <input type="submit" name="submit" value="Edit">
+                            <input type="hidden" name="id" value="' . $id . '" />
+                        </form>';
+                } else { // The record could not be validated
+                    echo '<p class="error">This page has been accessed in error</p>';
+                }
+                mysqli_close($dbcon);
+            ?>
+        </div><!-- End of the edit page content -->
     </div>
 </body>
 </html>
